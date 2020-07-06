0151ad6c94b68b8b1c33ea90046ef0de87f97524
chitransh01 <chitransh.jit@gmail.com>
2020-07-06 10:55:42 +0530

    Committed on 06-Jul-2020. Changes: #. Changed interval time of location tracking to 2 hours.

--- app/src/main/java/com/weather/app/view/constants/Constants.kt
+++ app/src/main/java/com/weather/app/view/constants/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val ID = "7778677"
     const val REQUEST_CODE_LOCATION = 1021
     const val REQUEST_CHECK_GPS_SETTINGS = 1022
-    const val LOCATION_INTERVAL = 10000L
+    const val LOCATION_INTERVAL = 2 * 60000L
     const val BASE_URL = "http://api.openweathermap.org/data/"
     const val WEATHER_URL = "2.5/weather"
 }
